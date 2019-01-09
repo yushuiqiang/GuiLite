144993f2d76dac296d4324450792e4b4863bd4d6
idea4good <idea4good@outlook.com>
2019-01-09 11:23:42 +0800

    !8 refactor wave_ctrl & support MCU basicly. Merge pull request !8 from idea4good/dev

--- core/core_include/surface.h
+++ core/core_include/surface.h
@@ -70,4 +70,22 @@ class c_surface_16bits : public c_surface {
 protected:
 	virtual void set_pixel(int x, int y, unsigned int rgb);
 };
+
+struct EXTERNAL_GFX_OP
+{
+	void (*draw_pixel)(int x, int y, unsigned int rgb);
+	void (*fill_rect)(int x0, int y0, int x1, int y1, unsigned int rgb);
+};
+class c_surface_mcu : public c_surface {
+	friend class c_display;
+	c_surface_mcu(c_display* display, unsigned int width, unsigned int height, unsigned int color_bytes, struct EXTERNAL_GFX_OP* gfx_op);
+	virtual void draw_pixel(int x, int y, unsigned int rgb, unsigned int z_order);
+	virtual void fill_rect(int x0, int y0, int x1, int y1, unsigned int rgb, unsigned int z_order);
+	virtual void fill_rect_on_fb(int x0, int y0, int x1, int y1, unsigned int rgb);
+	virtual unsigned int get_pixel(int x, int y, unsigned int z_order);
+protected:
+	virtual void set_pixel(int x, int y, unsigned int rgb);
+private:
+	struct EXTERNAL_GFX_OP* m_gfx_op;
+};
 #endif
