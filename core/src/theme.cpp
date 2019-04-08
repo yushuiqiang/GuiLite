53ce424766c88574dec2bb6b30c03596d63f0255
idea4good <idea4good@outlook.com>
2019-04-08 13:17:46 +0800

    !16 separate surface from c_wnd, merge core, gui into GuiLite

--- core/src/theme.cpp
+++ core/src/theme.cpp
@@ -1,13 +1,13 @@
 #include "core_include/api.h"
 #include "core_include/rect.h"
 #include "core_include/resource.h"
-#include "../gui_include/my_resource.h"
+#include "core_include/theme.h"
 
 static const FONT_INFO* s_font_map[FONT_MAX];
 static const BITMAP_INFO* s_bmp_map[BITMAP_MAX];
 static unsigned int s_color_map[COLOR_MAX];
 
-int c_my_resource::add_font(FONT_TYPE index, const FONT_INFO* font)
+int c_theme::add_font(FONT_TYPE index, const FONT_INFO* font)
 {
 	if (index >= FONT_MAX)
 	{
@@ -18,7 +18,7 @@ int c_my_resource::add_font(FONT_TYPE index, const FONT_INFO* font)
 	return 0;
 }
 
-const FONT_INFO* c_my_resource::get_font(FONT_TYPE index)
+const FONT_INFO* c_theme::get_font(FONT_TYPE index)
 {
 	if (index >= FONT_MAX)
 	{
@@ -28,7 +28,7 @@ const FONT_INFO* c_my_resource::get_font(FONT_TYPE index)
 	return s_font_map[index];
 }
 
-int c_my_resource::add_bitmap(BITMAP_TYPE index, const BITMAP_INFO* bmp)
+int c_theme::add_bitmap(BITMAP_TYPE index, const BITMAP_INFO* bmp)
 {
 	if (index >= BITMAP_MAX)
 	{
@@ -39,7 +39,7 @@ int c_my_resource::add_bitmap(BITMAP_TYPE index, const BITMAP_INFO* bmp)
 	return 0;
 }
 
-const BITMAP_INFO* c_my_resource::get_bmp(BITMAP_TYPE index)
+const BITMAP_INFO* c_theme::get_bmp(BITMAP_TYPE index)
 {
 	if (index >= BITMAP_MAX)
 	{
@@ -49,7 +49,7 @@ const BITMAP_INFO* c_my_resource::get_bmp(BITMAP_TYPE index)
 	return s_bmp_map[index];
 }
 
-int c_my_resource::add_color(COLOR_TYPE index, const unsigned int color)
+int c_theme::add_color(COLOR_TYPE index, const unsigned int color)
 {
 	if (index >= COLOR_MAX)
 	{
@@ -60,7 +60,7 @@ int c_my_resource::add_color(COLOR_TYPE index, const unsigned int color)
 	return 0;
 }
 
-const unsigned int c_my_resource::get_color(COLOR_TYPE index)
+const unsigned int c_theme::get_color(COLOR_TYPE index)
 {
 	if (index >= COLOR_MAX)
 	{
