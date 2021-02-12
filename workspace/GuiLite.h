87a64390a1dbe72f9aca4bd2e89074abd34964f1
yushuiqiang <yushuiqiang@aliyun.com>
2021-02-12 15:32:08 +0800

    修改cmake工程 去除Gradle工程

---
+++ workspace/GuiLite.h
@@ -0,0 +1,24 @@
+﻿#ifndef GUILITE_H
+#define GUILITE_H
+
+
+#include "./core_include/api.h"
+#include "./core_include/bitmap.h"
+#include "./core_include/display.h"
+#include "./core_include/resource.h"
+#include "./core_include/theme.h"
+#include "./core_include/wnd.h"
+#include "./core_include/word.h"
+#include "./widgets_include/button.h"
+#include "./widgets_include/dialog.h"
+#include "./widgets_include/edit.h"
+#include "./widgets_include/keyboard.h"
+#include "./widgets_include/label.h"
+#include "./widgets_include/list_box.h"
+#include "./widgets_include/slide_group.h"
+#include "./widgets_include/spinbox.h"
+#include "./widgets_include/table.h"
+#include "./widgets_include/wave_buffer.h"
+#include "./widgets_include/wave_ctrl.h"
+
+#endif //GUILITE_H
